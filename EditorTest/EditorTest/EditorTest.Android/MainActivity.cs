10254888f4bfac40550314352847d2a6faa65e35
Konstantin Kellermann <konstantin.kellermann@gmx.de>
2019-04-05 12:17:50 +0200

    Add files via upload

---
+++ EditorTest/EditorTest/EditorTest.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace EditorTest.Droid
+{
+    [Activity(Label = "EditorTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
